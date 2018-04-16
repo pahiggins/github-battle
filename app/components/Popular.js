cd7eb1b3123c12fa87f3eafe47f6ff91ff3113c2
Peter Higgins <pah@cenabo.com>
2018-04-16 12:00:08 +0100

    Add Battle and Popular features

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,6 +1,7 @@
 const React = require('react');
 const PropTypes = require('prop-types');
 const api = require('../utils/api');
+const Loading = require('./Loading');
 
 const SelectLanguage = (props) => {
   const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
@@ -86,7 +87,7 @@ class Popular extends React.Component {
           selectedLanguage={this.state.selectedLanguage}
           onSelect={this.updateLanguage}
         />
-        {!this.state.repos ? <p>Loading...</p> : <RepoGrid repos={this.state.repos}/>}
+        {!this.state.repos ? <Loading /> : <RepoGrid repos={this.state.repos}/>}
       </div>
     );
   }
